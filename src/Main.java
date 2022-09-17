e48d01f342799024757482f737c2dd0052e6b406
Oleksandr <s4ny4best@gmail.com>
2022-09-17 12:52:54 +0300

    наповнив клас Result

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        //Employee massage = new Employee("Дмитрук О.В ", " Інженер");
-       // Employee massage2 = new Employee("Гуцалюк А.В ", " Інженер");
-        // System.out.println(massage.PQ());
-       // System.out.println(massage2.PQ());
-        Device gadJet = new Device("Dell","P75", 3, 21) ;
-        String[] e = gadJet.getProblemType();
-        for (int i = 0;i<3;i++)
-        {
-        System.out.println(e[i]);
-        }
+        Employee massage = new Employee("Дмитрук О.В ", " Інженер");
+        Employee massage2 = new Employee("Гуцалюк А.В ", " Інженер");
+
+        System.out.println(massage.PQ());
+        System.out.println(massage2.PQ());
+        System.out.println(massage2.PQ());
     }
     //yu
 }
