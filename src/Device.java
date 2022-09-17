ad29c92e1590f63302df09529f21442e72928287
Narestur <artemgutsal@gmail.com>
2022-09-17 11:18:17 +0300

    Добавлено Device поля.

--- src/Device.java
+++ src/Device.java
@@ -3,4 +3,38 @@ public class Device {
     private String model;
     private String problemType;
     private int requestNumber;
+
+    public String getName ()
+    {
+        return name;
+    }
+    public String getModel ()
+    {
+        return model;
+    }
+    public String getProblemType()
+    {
+        return problemType;
+    }
+    public int getRequestNumber()
+    {
+        return requestNumber;
+    }
+    public void setRequestNumber(int requestNumber)
+    {
+        this.requestNumber=requestNumber;
+    }
+    public void setName (String name)
+    {
+        this.name=name;
+    }
+    public void setModel(String model)
+    {
+        this.model = model;
+    }
+    public void setProblemType(String problemType)
+    {
+        this.problemType = problemType;
+    }
+
 }
