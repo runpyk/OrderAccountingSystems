4792584a498f04eff74179ec7ec37d6f1454c901
Oleksandr <s4ny4best@gmail.com>
2022-09-19 16:09:58 +0300

    наповнив клас main file

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import java.io.IOException;
+
+import java.io.*;
+
+import java.util.ArrayList;
 public class Main {
-    public static void main(String[] args) {
-        //Employee massage = new Employee("Дмитрук О.В ", " Інженер");
-       // Employee massage2 = new Employee("Гуцалюк А.В ", " Інженер");
-        // System.out.println(massage.PQ());
-       // System.out.println(massage2.PQ());
+    public static void main(String[] args) throws IOException {
+
         Device gadJet = new Device("Dell","P75", 3, 21) ;
         String[] e = gadJet.getProblemType();
         System.out.println("Проблема: ");
+
+        File file = new File("problem.txt");
+        FileWriter writer = new FileWriter(file.getAbsolutePath(),true);
+        writer.write("HASHSET");
+
+        writer.close();
+        FileReader reader = new FileReader(file.getAbsolutePath());
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String fileStrings;
+        ArrayList<String> arrayList = new ArrayList<>();
+        while( (fileStrings = bufferedReader.readLine()) != null) {
+            arrayList.add(fileStrings);
+        }
         for (int i = 0;i<3;i++)
         {
         System.out.println(e[i]);
