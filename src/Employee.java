f458baaad182ecc6d614c0ba5dbad11cd1b4f2e9
Oleksandr <s4ny4best@gmail.com>
2022-09-25 14:37:01 +0300

    наповнив клас emp

--- src/Employee.java
+++ src/Employee.java
@@ -1,37 +1,51 @@
-public class Employee {
-    private String PIB;
-    private String qualification;
+abstract class Employee {
+     String PIB;
+     String qualification;
+     public abstract void Work();
+    }
 
-    public Employee(String PIB, String qualification){
+    class Acceptor extends Employee{
+    public Acceptor(String PIB){
         this.PIB = PIB;
-        this.qualification = qualification;
-
+        this.qualification = "Acceptor";
     }
-    public Employee(){
-
+    public void Work(){
     }
-
     public void setPIB(String PIB) {
         this.PIB = PIB;
     }
-
     public String getPIB() {
         return PIB;
     }
-
-    public void setQualification(String qualification) {
-        this.PIB = qualification;
-    }
-
     public String getQualification() {
         return qualification;
     }
-
     public String PQ() {
         String message;
         message = "ПІБ робітника: " + this.PIB + "Кваліфікація: " + this.qualification;
-
         return message;
+    }
+}
 
+class Engineer extends Employee{
+    public Engineer(String PIB){
+        this.PIB = PIB;
+        this.qualification = "Engineer";
+    }
+    public void Work(){
+    }
+    public void setPIB(String PIB) {
+        this.PIB = PIB;
+    }
+    public String getPIB() {
+        return PIB;
+    }
+    public String getQualification() {
+        return qualification;
+    }
+    public String PQ() {
+        String message;
+        message = "ПІБ робітника: " + this.PIB + "Кваліфікація: " + this.qualification;
+        return message;
     }
 }
